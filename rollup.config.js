357c2c8cf771d0732d81f9d6ddbe3e4044f97e15
Restyled.io <commits@restyled.io>
2023-01-31 17:31:35 +0000

    Restyled by clang-format

--- rollup.config.js
+++ rollup.config.js
@@ -4,9 +4,10 @@ import sucrase from "@rollup/plugin-sucrase";
 import buble from "rollup-plugin-buble";
 import common from "rollup-plugin-commonjs";
 import scss from "rollup-plugin-scss";
-import { terser } from "rollup-plugin-terser";
+import {terser} from "rollup-plugin-terser";
 import vue from "rollup-plugin-vue";
-import pkg from "./package.json" assert { type: "json" };
+
+import pkg from "./package.json" assert {type : "json"};
 
 const banner = `/*
  * ${pkg.name}
@@ -18,50 +19,52 @@ const banner = `/*
 
 export default {
   input: "src/index.js",
-  output: [
-    {
-      file: pkg.main,
-      format: "cjs",
-      exports: "named",
-      strict: true,
-      banner,
-    },
-    {
-      file: pkg.module,
-      format: "es",
-      exports: "named",
-      strict: true,
-      banner,
-    },
-    {
-      file: pkg.umd,
-      format: "umd",
-      exports: "named",
-      strict: true,
-      banner,
-      name: "FloatMenu",
-      globals: {
-        vue: "vue",
-      },
-    },
-  ],
-  plugins: [
-    scss(),
-    vue(),
-    sucrase({
-      exclude: ["node_modules/**"],
-      transforms: ["typescript"],
-    }),
-    beep(),
-    common(),
-    buble(),
-    resolve(),
-    terser({
-      compress: {
-        drop_console: true,
-        drop_debugger: true,
-      },
-    }),
-  ],
-  external: ["vue"],
+  output:
+      [
+        {
+          file : pkg.main,
+          format : "cjs",
+          exports : "named",
+          strict : true,
+          banner,
+        },
+        {
+          file : pkg.module,
+          format : "es",
+          exports : "named",
+          strict : true,
+          banner,
+        },
+        {
+          file : pkg.umd,
+          format : "umd",
+          exports : "named",
+          strict : true,
+          banner,
+          name : "FloatMenu",
+          globals : {
+            vue : "vue",
+          },
+        },
+      ],
+  plugins:
+      [
+        scss(),
+        vue(),
+        sucrase({
+          exclude : [ "node_modules/**" ],
+          transforms : [ "typescript" ],
+        }),
+        beep(),
+        common(),
+        buble(),
+        resolve(),
+        terser({
+          compress : {
+            drop_console : true,
+            drop_debugger : true,
+          },
+        }),
+      ],
+  external: [ "vue" ],
 };
